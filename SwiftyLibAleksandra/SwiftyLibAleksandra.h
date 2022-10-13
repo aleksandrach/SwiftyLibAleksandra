f13560f9cfb7190bfad227fe50e1aad9491459cf
Aleksandra <a.chekichevska@axeltra.com>
2022-10-13 09:19:23 +0200

    Initial project setup

---
+++ SwiftyLibAleksandra/SwiftyLibAleksandra.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyLibAleksandra.h
+//  SwiftyLibAleksandra
+//
+//  Created by Aleksandra Chekichevska on 13.10.22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLibAleksandra.
+FOUNDATION_EXPORT double SwiftyLibAleksandraVersionNumber;
+
+//! Project version string for SwiftyLibAleksandra.
+FOUNDATION_EXPORT const unsigned char SwiftyLibAleksandraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLibAleksandra/PublicHeader.h>
+
+
